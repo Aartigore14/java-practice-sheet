fb4d387f295d10dab683961a9837049b5aad022a
Aarti Gore <aartigore2005@gmail.com>
2026-05-28 16:43:14 +0530

    Implement default methods in Media interface for backward compatibility

---
+++ src/OOP/Main.java
@@ -0,0 +1,22 @@
+package OOP;
+//default implementation
+interface Media{
+    abstract void play();
+    default void pause(){
+        System.out.println("pause the video(default implementation)");
+    }
+}
+class VideoPlayer implements basics.Media {
+    @Override
+    public void play() {
+        System.out.println("video is playing!!");
+    }
+}
+public class Main {
+    static void main() {
+        VideoPlayer v = new VideoPlayer();
+        v.pause();
+        v.play();
+    }
+}
+
