df2d78220e3b9f7140a564273e59f7dc37f8e607
Aarti Gore <aartigore2005@gmail.com>
2026-05-29 14:43:48 +0530

    Implement in-place linear-time zero shifting logic for arrays

---
+++ src/Arrays/MoveZeros.java
@@ -0,0 +1,29 @@
+package Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MoveZeros {
+    static void main() {
+        Scanner sc  = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.print("Enter Array elements: ");
+        for (int i = 0; i < arr.length ; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int count=0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]!=0){
+                arr[count]=arr[i];
+                count++;
+            }
+        }
+        while (count<arr.length){
+            arr[count]=0;
+            count++;
+        }
+        System.out.println("Array after moving zeros: "+ Arrays.toString(arr));
+    }
+}
